175ec4d4d2e8c1f88d93b2590e0e4191fdbafffb
Vlad_Lukyanchuk <vladyslav.lukyanchuk@gmail.com>
2022-12-31 00:21:31 +0200

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <div class="wrapper">
+    <header class="header">
+      <div class="header__content">
+        <div class="header__left-side">
+          <a href="" class="header__burger">
+            <box-icon class="burger-icon" color="#101010" name="menu"></box-icon>
+          </a>
+        </div>
+        <div class="header__center">
+          <a href="/" class="header__logo">logo.</a>
+        </div>
+
+        <div class="header__right-side">
+          <ul class="header__links">
+            <li>
+              <a href="/log-in" class="header__log-in"> Log In </a>
+            </li>
+          </ul>
+          <a href="/shop-bag" class="shop-bag">
+            <box-icon
+              class="shop-bag__icon"
+              color="#101010"
+              name="shopping-bag"
+            ></box-icon>
+          </a>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@/assets/scss/main.scss'
+
+import 'boxicons'
+
 const app = createApp(App);
 
 app.mount('#app')
